c9da72cb84cd284725de5e1a8b1afa828a4ae707
Chloe J. Taylor <44882011+steakbaby2@users.noreply.github.com>
2021-04-29 18:36:55 -0400

    Finished Console Model

--- ZaidAbdul-KaudeyrChloeTaylorCapstone/src/main/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/dao/ConsoleDao.java
+++ ZaidAbdul-KaudeyrChloeTaylorCapstone/src/main/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/dao/ConsoleDao.java
@@ -1,4 +1,20 @@
 package com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.dao;
 
+import com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.model.Console;
+
+import java.util.List;
+
 public interface ConsoleDao {
+
+    Console addConsole(Console console);
+
+    Console getConsole(int id);
+
+    List<Console> getAllConsoles();
+
+    List<Console> getConsolesByManufacturer(String manufacturer);
+
+    void updateConsole(Console console);
+
+    void deleteConsole(int id);
 }
