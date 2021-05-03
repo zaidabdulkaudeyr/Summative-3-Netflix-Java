c090a39e068224f9a8e7fdaa4d1c370e0ff39377
Chloe J. Taylor <44882011+steakbaby2@users.noreply.github.com>
2021-05-03 19:59:20 -0400

    Updated ConsoleDaoJdbcTemplateImpl

--- ZaidAbdul-KaudeyrChloeTaylorCapstone/src/main/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/dao/ConsoleDaoJdbcTemplateImpl.java
+++ ZaidAbdul-KaudeyrChloeTaylorCapstone/src/main/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/dao/ConsoleDaoJdbcTemplateImpl.java
@@ -7,6 +7,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Repository
@@ -43,39 +45,68 @@ public class ConsoleDaoJdbcTemplateImpl implements ConsoleDao {
 
         jdbcTemplate.update(
                 INSERT_CONSOLE_SQL,
-                //Add this later
+                console.getModel(),
+                console.getManufacturer(),
+                console.getMemoryAmount(),
+                console.getProcessor(),
+                console.getPrice(),
+                console.getQuantity());
 
         int id = jdbcTemplate.queryForObject("select LAST_INSERT_ID()", Integer.class);
-
         console.setId(id);
-
         return console;
     }
 
     @Override
     public Console getConsole(int id) {
-        return null;
+        try
+        {
+            return jdbcTemplate.queryForObject(SELECT_CONSOLE_SQL, this::mapRowToConsole, id);
+        } catch (EmptyResultDataAccessException e)
+        {
+            // if nothing is returned just catch the exception and return null
+            return null;
+        }
     }
 
     @Override
     public List<Console> getAllConsoles() {
-        return null;
+        return jdbcTemplate.query(SELECT_ALL_CONSOLE_SQL, this::mapRowToConsole);
     }
 
     @Override
     public List<Console> getConsolesByManufacturer(String manufacturer) {
-        return null;
+        return jdbcTemplate.query(SELECT_CONSOLES_BY_MANUFACTURER_SQL, this::mapRowToConsole, manufacturer);
     }
 
     @Override
     public void updateConsole(Console console) {
-
+        jdbcTemplate.update(UPDATE_CONSOLE_SQL,
+                console.getModel(),
+                console.getManufacturer(),
+                console.getMemoryAmount(),
+                console.getProcessor(),
+                console.getPrice(),
+                console.getQuantity());
     }
 
     @Override
     public void deleteConsole(int id){
-
+        jdbcTemplate.update(DELETE_CONSOLE_SQL, id);
     }
 
+    //row mapper
+    private Console mapRowToConsole(ResultSet rs, int rowNum) throws SQLException
+    {
+        Console console = new Console();
+        console.setId(rs.getInt("console_id"));
+        console.setModel(rs.getString("model"));
+        console.setManufacturer(rs.getString("manufacturer"));
+        console.setMemoryAmount(rs.getString("memory_amount"));
+        console.setProcessor(rs.getString("processor"));
+        console.setPrice(rs.getBigDecimal("price"));
+        console.setQuantity(rs.getInt("quantity"));
+        return console;
+    }
 
 }
