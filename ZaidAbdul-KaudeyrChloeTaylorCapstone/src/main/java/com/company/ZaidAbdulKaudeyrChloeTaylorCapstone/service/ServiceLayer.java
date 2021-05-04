044f6bac96e5820938872b600ad9b5c384740962
Chloe J. Taylor <44882011+steakbaby2@users.noreply.github.com>
2021-05-04 17:52:48 -0400

    Did stuff in the service layer

--- ZaidAbdul-KaudeyrChloeTaylorCapstone/src/main/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/service/ServiceLayer.java
+++ ZaidAbdul-KaudeyrChloeTaylorCapstone/src/main/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/service/ServiceLayer.java
@@ -1,4 +1,22 @@
 package com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.service;
 
+import com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.dao.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ServiceLayer {
+    private ConsoleDao consoleDao;
+    private GameDao gameDao;
+    private TshirtDao tshirtDao;
+    private InvoiceDao invoiceDao;
+    private ProcessingFeeDao processingFeeDao;
+    private TaxDao taxDao;
+
+    @Autowired
+    public ServiceLayer(ConsoleDao consoleDao, GameDao gameDao, TshirtDao tshirtDao, InvoiceDao invoiceDao, ProcessingFeeDao processingFeeDao, TaxDao taxDao) {
+        this.consoleDao = consoleDao;
+        this.gameDao = gameDao;
+
+    }
 }
