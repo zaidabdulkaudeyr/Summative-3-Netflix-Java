d6ec70f27d6c6a3dc232abf908ff2513da0c2ef8
Chloe J. Taylor <44882011+steakbaby2@users.noreply.github.com>
2021-05-03 22:38:23 -0400

    Added game test

---
+++ ZaidAbdul-KaudeyrChloeTaylorCapstone/src/test/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/dao/GameDaoJdbcTemplateImplTest.java
@@ -0,0 +1,40 @@
+package com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameDaoJdbcTemplateImplTest {
+
+    @Test
+    void addGame() {
+    }
+
+    @Test
+    void getGame() {
+    }
+
+    @Test
+    void getAllGames() {
+    }
+
+    @Test
+    void updateGame() {
+    }
+
+    @Test
+    void deleteGame() {
+    }
+
+    @Test
+    void getGamesByStudio() {
+    }
+
+    @Test
+    void getGamesByESRB() {
+    }
+
+    @Test
+    void getGamesByTitle() {
+    }
+}
