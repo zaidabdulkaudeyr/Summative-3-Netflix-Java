e3b35f9c7e739c619ce6a7005be7db935d21b818
Chloe J. Taylor <44882011+steakbaby2@users.noreply.github.com>
2021-05-04 09:53:12 -0400

    Adding in Tshirt Test dao

---
+++ ZaidAbdul-KaudeyrChloeTaylorCapstone/src/test/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/dao/TshirtDaoTest.java
@@ -0,0 +1,37 @@
+package com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TshirtDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getTshirt() {
+    }
+
+    @Test
+    void getAllTshirts() {
+    }
+
+    @Test
+    void getTshirtByColor() {
+    }
+
+    @Test
+    void getTshirtBySize() {
+    }
+
+    @Test
+    void updateTshirt() {
+    }
+
+    @Test
+    void deleteTshirt() {
+    }
+}
