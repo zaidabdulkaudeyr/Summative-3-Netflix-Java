af7578c7369ba8088076ec97f6849eccabf3d474
Chloe J. Taylor <44882011+steakbaby2@users.noreply.github.com>
2021-05-03 22:37:23 -0400

    Added comany test

---
+++ ZaidAbdul-KaudeyrChloeTaylorCapstone/src/test/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/dao/ConsoleDaoJdbcTemplateImplTest.java
@@ -0,0 +1,32 @@
+package com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.dao;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsoleDaoJdbcTemplateImplTest {
+
+    @Test
+    void addConsole() {
+    }
+
+    @Test
+    void getConsole() {
+    }
+
+    @Test
+    void getAllConsoles() {
+    }
+
+    @Test
+    void getConsolesByManufacturer() {
+    }
+
+    @Test
+    void updateConsole() {
+    }
+
+    @Test
+    void deleteConsole() {
+    }
+}
