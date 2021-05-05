66e8dce27c649fe0b2e6a07363e2eebbdfcd8ddf
Chloe J. Taylor <44882011+steakbaby2@users.noreply.github.com>
2021-05-04 20:48:51 -0400

    Changed invoice stuffs

--- ZaidAbdul-KaudeyrChloeTaylorCapstone/src/test/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/dao/InvoiceDaoTest.java
+++ ZaidAbdul-KaudeyrChloeTaylorCapstone/src/test/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/dao/InvoiceDaoTest.java
@@ -39,10 +39,10 @@ public class InvoiceDaoTest {
         invoice.setCity("Norfolk");
         invoice.setState("VA");
         invoice.setZipcode("23407");
-        invoice.setItemType("Console");
-        invoice.setItemId(1); //Found in service layer
+        invoice.setItemType("Console"); //User puts this in need error handling
+        invoice.setItemId(1); //User puts this in need error handling
         invoice.setUnitPrice(new BigDecimal("299.00")); //Found in service layer
-        invoice.setQuantity(1);
+        invoice.setQuantity(1); //User puts this in need error handling
         invoice.setSubtotal(new BigDecimal("304.99")); //Calculated in service layer
         invoice.setTax(new BigDecimal(".06")); //Found in service Layer
         invoice.setProcessingFee(new BigDecimal("14.99")); //Found in service layer
@@ -65,5 +65,44 @@ public class InvoiceDaoTest {
     @Test
     public void getAllInvoices() {
 
+        //First invoice
+        Invoice invoice = new Invoice();
+        invoice.setName("Amanda");
+        invoice.setStreet("12345 Presidential Lane");
+        invoice.setCity("Norfolk");
+        invoice.setState("VA");
+        invoice.setZipcode("23407");
+        invoice.setItemType("Console");
+        invoice.setItemId(1); //Found in service layer
+        invoice.setUnitPrice(new BigDecimal("299.00")); //Found in service layer
+        invoice.setQuantity(1);
+        invoice.setSubtotal(new BigDecimal("304.99")); //Calculated in service layer
+        invoice.setTax(new BigDecimal(".06")); //Found in service Layer
+        invoice.setProcessingFee(new BigDecimal("14.99")); //Found in service layer
+        invoice.setTotal(new BigDecimal("316.00")); //Found in service layer
+
+        invoice = invoiceDao.addInvoice(invoice);
+
+        //Second invoice
+        Invoice invoice1 = new Invoice();
+        invoice1.setName("Amanda");
+        invoice1.setStreet("12345 Presidential Lane");
+        invoice1.setCity("Norfolk");
+        invoice1.setState("VA");
+        invoice1.setZipcode("23407");
+        invoice1.setItemType("Console");
+        invoice1.setItemId(1); //Found in service layer
+        invoice1.setUnitPrice(new BigDecimal("299.00")); //Found in service layer
+        invoice1.setQuantity(1);
+        invoice1.setSubtotal(new BigDecimal("304.99")); //Calculated in service layer
+        invoice1.setTax(new BigDecimal(".06")); //Found in service Layer
+        invoice1.setProcessingFee(new BigDecimal("14.99")); //Found in service layer
+        invoice1.setTotal(new BigDecimal("316.00")); //Found in service layer
+
+        invoice1 = invoiceDao.addInvoice(invoice1);
+
+        List<Invoice> bList = invoiceDao.getAllInvoices();
+
+        assertEquals(bList.size(), 2);
     }
 }
