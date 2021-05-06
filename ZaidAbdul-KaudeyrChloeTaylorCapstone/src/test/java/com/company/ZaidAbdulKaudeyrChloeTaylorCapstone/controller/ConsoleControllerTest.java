ae0d2eb8569459c10b55033babe4b472d9a75931
Zaid <z.i.abdul-kaudeyr@spartans.nsu.edu>
2021-05-05 22:23:50 -0400

    some tests

---
+++ ZaidAbdul-KaudeyrChloeTaylorCapstone/src/test/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/controller/ConsoleControllerTest.java
@@ -0,0 +1,32 @@
+package com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConsoleControllerTest {
+
+    @Test
+    public void getAllConsoles() {
+    }
+
+    @Test
+    public void getConsolesByManufacturer() {
+    }
+
+    @Test
+    public void getConsole() {
+    }
+
+    @Test
+    public void createConsole() {
+    }
+
+    @Test
+    public void updateConsole() {
+    }
+
+    @Test
+    public void deleteConsole() {
+    }
+}
---
+++ ZaidAbdul-KaudeyrChloeTaylorCapstone/src/test/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/controller/GameControllerTest.java
@@ -0,0 +1,32 @@
+package com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameControllerTest {
+
+    @Test
+    public void getAllGames() {
+    }
+
+    @Test
+    public void getGamesByStudio() {
+    }
+
+    @Test
+    public void getGamesByESRB() {
+    }
+
+    @Test
+    public void getGamesByTitle() {
+    }
+
+    @Test
+    public void getGame() {
+    }
+
+    @Test
+    public void addGame() {
+    }
+}
