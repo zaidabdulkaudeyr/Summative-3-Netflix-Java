8fe8453c1f9082e6138d738107acf92a0ae7da18
Chloe J. Taylor <44882011+steakbaby2@users.noreply.github.com>
2021-05-05 22:34:01 -0400

    Added InvoiceController

--- ZaidAbdul-KaudeyrChloeTaylorCapstone/src/main/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/controller/InvoiceController.java
+++ ZaidAbdul-KaudeyrChloeTaylorCapstone/src/main/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/controller/InvoiceController.java
@@ -1,4 +1,23 @@
 package com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.controller;
 
+import com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.service.ServiceLayer;
+import com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.viewmodel.InvoiceViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class InvoiceController {
+
+    @Autowired
+    ServiceLayer service;
+
+    @PostMapping(value = "/invoice")
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public InvoiceViewModel addInvoice(@RequestBody InvoiceViewModel viewModel) {
+        return service.addInvoice(viewModel);
+    }
 }
