f3b8b491d38fa6da407e91b3ff452bf1c2d225be
Chloe J. Taylor <44882011+steakbaby2@users.noreply.github.com>
2021-05-06 15:12:43 -0400

    Did stuff

--- ZaidAbdul-KaudeyrChloeTaylorCapstone/src/main/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/controller/TshirtController.java
+++ ZaidAbdul-KaudeyrChloeTaylorCapstone/src/main/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/controller/TshirtController.java
@@ -1,60 +1,77 @@
 package com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.controller;
 
+import com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.dao.TshirtDao;
+
 import com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.model.Tshirt;
-import com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.service.ServiceLayer;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 public class TshirtController {
 
-    @Autowired
-    ServiceLayer service;
 
-    @GetMapping(value = "/tshirt")
-    @ResponseStatus(value = HttpStatus.OK)
-    public List<Tshirt> getAllTshirts() {
-        List<Tshirt> tshirts = service.getAllTshirts();
-        return tshirts;
+
+    //@Autowired
+    //ServiceLayer service;
+    @Autowired
+    TshirtDao tshirtDao;
+    public TshirtController(TshirtDao dao){this.tshirtDao = tshirtDao;}
+
+    //@GetMapping(value = "/tshirt")
+    //@ResponseStatus(value = HttpStatus.OK)
+    //public List<Tshirt> getAllTshirts() {
+    //    List<Tshirt> tshirts = tshirt.getAllTshirts();
+    //    return tshirts;
+    //}
+
+    @RequestMapping(value = "/tshirt", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public List<Tshirt> getAllTshirt(){
+        System.out.println("Getting all tshirts...");
+        return tshirtDao.getAllTshirts();
     }
 
     @RequestMapping(value = "/tshirtColor/{color}", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public List<Tshirt> getTshirtByColor(@PathVariable String color) {
-        return service.getTshirtByColor(color);
+        return tshirtDao.getTshirtByColor(color);
     }
 
     @RequestMapping(value = "/tshirtSize/{color}", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public List<Tshirt> getTshirtBySize(@PathVariable String size) {
-        return service.getTshirtBySize(size);
+        return tshirtDao.getTshirtBySize(size);
     }
 
     @RequestMapping(value = "/tshirt/{id}", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.OK)
     public Tshirt getTshirt(@PathVariable int id) {
-        return service.getTshirt(id);
+        return tshirtDao.getTshirt(id);
     }
 
     @PostMapping(value = "/tshirt")
     @ResponseStatus(value = HttpStatus.CREATED)
     public Tshirt addTshirt(@RequestBody Tshirt tshirt) {
-        return service.addTshirt(tshirt);
+        System.out.println("creating tshirt");
+        return tshirtDao.addTshirt(tshirt);
     }
 
     @PutMapping(value = "/tshirt/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void updateTshirt(@PathVariable int id, @RequestBody Tshirt tshirt) {
         tshirt.setId(id);
-        service.updateTshirt(tshirt);
+        tshirtDao.updateTshirt(tshirt);
     }
 
     @DeleteMapping(value = "/tshirt/{id}")
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteTshirt(@PathVariable int id) {
-        service.deleteTshirt(id);
+        tshirtDao.deleteTshirt(id);
     }
 }
--- ZaidAbdul-KaudeyrChloeTaylorCapstone/src/test/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/controller/TshirtControllerTest.java
+++ ZaidAbdul-KaudeyrChloeTaylorCapstone/src/test/java/com/company/ZaidAbdulKaudeyrChloeTaylorCapstone/controller/TshirtControllerTest.java
@@ -1,19 +1,27 @@
 package com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.controller;
 
+import com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.dao.TshirtDao;
 import com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.model.Tshirt;
+import com.company.ZaidAbdulKaudeyrChloeTaylorCapstone.service.ServiceLayer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -27,15 +35,24 @@ public class TshirtControllerTest {
     // ObjectMapper used to convert Java objects to JSON and vise versa
     private ObjectMapper mapper = new ObjectMapper();
 
-    // A list of records for testing purposes
-    private List<Tshirt> tshirtList;
+    // A list of tshirts for testing purposes
+    //private List<Tshirt> tshirtList;
 
-    // Testing GET /records
+    @MockBean
+    TshirtDao tshirtList;
+
+    @Before
+    public void setUp() {
+        // Standard set up method, for instantiating test object
+        // don't have to do anything special for mockMvc since it's autowired
+    }
+
+
+    // Testing GET
     @Test
     public void shouldReturnAllTshirts() throws Exception  {
         // ARRANGE
         // Convert Java object to JSON
-
         String outputJson = mapper.writeValueAsString(tshirtList);
 
         //ACT
@@ -53,14 +70,44 @@ public class TshirtControllerTest {
     }
 
     @Test
-    public void getTshirt() {
+    public void shouldReturnTshirtById() throws Exception{
+        Tshirt outputTshirt = new Tshirt();
+        outputTshirt.setSize("M");
+        outputTshirt.setColor("pink");
+        outputTshirt.setDescription("Very Pretty");
+        outputTshirt.setPrice(new BigDecimal("19.99"));
+        outputTshirt.setQuantity(5);
+
+        String outputJson = mapper.writeValueAsString(outputTshirt);
+
+        mockMvc.perform(get("/tshirt/2"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(outputJson));
     }
 
     @Test
-    public void addTshirt() {
+    public void shouldReturnNewRecordTshirt() throws Exception{
+        // Arrange
+        Tshirt tshirt = new Tshirt();
+        tshirt.setSize("M");
+        tshirt.setColor("Pink");
+        tshirt.setDescription("Super Soft and Ultra Plush");
+        tshirt.setPrice(new BigDecimal("19.99"));
+        tshirt.setQuantity(10);
+
+        // Convert Java to JSON
+        String inputJson = mapper.writeValueAsString(tshirt);
     }
 
     @Test
     public void updateTshirt() {
+    }
+
+    @Test
+    public void shouldDeleteByIdTshirt() throws Exception {
+        mockMvc.perform(delete("/tshirt/2"))
+                .andDo(print())
+                .andExpect(status().isNoContent());
     }
 }
